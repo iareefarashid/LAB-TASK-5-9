bf5211ead9e17437a7c2df5db64f04af3cd6b8ad
AREEFA RASHID <115347237+iareefarashid@users.noreply.github.com>
2022-10-29 14:27:39 +0500

    Update RestaurantMenu.java

--- RestaurantMenu.java
+++ RestaurantMenu.java
@@ -11,12 +11,12 @@ class RestaurantMenu {
         System.out.println("\n\t\t\tH&Y Restaurant");
         System.out.println("\t---------------Menue---------------");
         System.out.println("\n");
-        System.out.println("\t\t1.Biryani:");
+        System.out.println("\t\t1.Chicken Biryani:");
         System.out.println("\t\t2.Shami Kabab:");
         System.out.println("\t\t3.Nihari");
-        System.out.println("\t\t4.:beef biryani");
-        System.out.println("\t\t5.mutton karhai:");
-        System.out.println("\t\t6.Chiken Karhai:");
+        System.out.println("\t\t4.:Beef biryani");
+        System.out.println("\t\t5.Mutton karhai:");
+        System.out.println("\t\t6.Chicken Karhai:");
         System.out.println("\t\t7.Naan:");
         System.out.println("\t\t8.Cold drink:");
         System.out.println("\n\tSelect menu from 1 to 8 for Price of Menu:");
@@ -24,8 +24,8 @@ class RestaurantMenu {
         switch (menuitem) {
             case 1: {
                 ;
-                order = "\tBiryani";
-                System.out.println("Biryani = 120Rs");
+                order = "\tChicken Biryani";
+                System.out.println("chicken Biryani = 120Rs");
             }
                 break;
             case 2: {
@@ -39,18 +39,18 @@ class RestaurantMenu {
             }
                 break;
             case 4: {
-                order = "\tbeef biryani";
-                System.out.println("\tbeef biryani = 250Rs");
+                order = "\tBeef Biryani";
+                System.out.println("\tBeef Biryani = 250Rs");
             }
                 break;
             case 5: {
-                order = "\tmutton karhai";
-                System.out.println("\tmutton karhai = 2000Rs");
+                order = "\tMutton karhai";
+                System.out.println("\tMutton karhai = 2000Rs");
             }
                 break;
             case 6: {
-                order = "\tChiken karhai";
-                System.out.println("\tChiken Karhai = 1200Rs");
+                order = "\tChicken karhai";
+                System.out.println("\tChicken Karhai = 1200Rs");
             }
                 break;
             case 7: {
